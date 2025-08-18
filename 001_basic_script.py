b15507eb6b531997c5e346c6659e792279a5d7f4
zy mac <zhenyuan992@github.com>
2025-08-18 18:22:12 +0800

    add

---
+++ 001_basic_script.py
@@ -0,0 +1,10 @@
+import nd2
+import trackpy as tp
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
+import warnings
+warnings.filterwarnings('ignore')
+
+print("hello")
